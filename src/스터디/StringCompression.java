9d3cca9c48726beebc58c97a05e76b6261aae770
이승주 <112889287+WinsomeJoo@users.noreply.github.com>
2024-07-04 14:16:13 +0900

    [study] 회문 문자열

--- src/스터디/StringCompression.java
+++ src/스터디/StringCompression.java
@@ -1,5 +1,10 @@
 package 스터디;
 import java.util.*;
+
+/**알파벳 대문자로 이루어진 문자열을 입력받아 같은 문자가 연속으로 반복되는 경우 반복되는
+  문자 바로 오른쪽에 반복 횟수를 표기하는 방법으로 문자열을 압축하는 프로그램을 작성하세요.
+  단 반복횟수가 1인 경우 생략합니다.*/
+
 class StringCompression {
     public String solution(String a){
         String answer="";
